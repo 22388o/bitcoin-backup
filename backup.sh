b6eba29c813634ca3ed23c21e86e5002ab65de4c
アレフ <93302536+leffw@users.noreply.github.com>
2022-08-23 10:08:49 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,35 +3,28 @@
 # Check if inotify-tools is installed otherwise
 # it installs automatically.
 if ! which inotifywait &> /dev/null ; then
-    apt-get install inotify-tools -y
+    sudo apt-get install inotify-tools -y
 fi
 
 mkdir -p ~/.bk
-if [ ! -f "/media/$label/wallet.dat" ]; then
+if [ ! -f $(echo ~/.bk/configs.json) ]; then
+    echo "Configure the configuration file in: ~/.bk/configs.json"
     cp ./configs.json ~/.bk/configs.json
+    exit
 fi
 
 # Get the label of the device that 
 # will be used as a backup.
 echo $(date +"%Y-%m-%d-%H:%M:%S") "Loading configuration file."
 label=$(cat ~/.bk/configs.json | jq -r ."device")
-if [ !label ]; then
-    echo $(date +"%Y-%m-%d-%H:%M:%S")  "You need to configure the configuration file in: ~/.bk/configs.json."
-    exit
-fi
-
 wallet=$(cat ~/.bk/configs.json | jq -r ."wallet")
-if [ !label ]; then
-    echo $(date +"%Y-%m-%d-%H:%M:%S")  "You need to configure the configuration file in: ~/.bk/configs.json."
-    exit
-fi
 
 # Search for the device with the 
 # configuration file label.
 echo $(date +"%Y-%m-%d-%H:%M:%S") "Searching device: $label." 
 device=$(blkid | grep $label | awk -F':' '{print $1}') 
 
-if mount | grep $device &> /dev/null; then
+if findmnt /media/$label > /dev/null; then
     echo $(date +"%Y-%m-%d-%H:%M:%S") "Backup device already mounted."
 else 
     mkdir -p /media/$label
@@ -43,5 +36,5 @@ while true; do
         cp $wallet /media/$label
     fi
     inotifywait $wallet
-    cp $wallet /media/$label
+    cp -r $wallet /media/$label
 done
