2a0c0ca35e84680e881a2e3574e3ee6af20c6a28
アレフ <93302536+leffw@users.noreply.github.com>
2022-08-23 09:39:33 -0300

    First commit

---
+++ backup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Check if inotify-tools is installed otherwise
+# it installs automatically.
+if ! which inotifywait &> /dev/null ; then
+    apt-get install inotify-tools -y
+fi
+
+mkdir -p ~/.bk
+if [ ! -f "/media/$label/wallet.dat" ]; then
+    cp ./configs.json ~/.bk/configs.json
+fi
+
+# Get the label of the device that 
+# will be used as a backup.
+echo $(date +"%Y-%m-%d-%H:%M:%S") "Loading configuration file."
+label=$(cat ~/.bk/configs.json | jq -r ."device")
+wallet=$(cat ~/.bk/configs.json | jq -r ."wallet")
+
+# Search for the device with the 
+# configuration file label.
+echo $(date +"%Y-%m-%d-%H:%M:%S") "Searching device: $label." 
+device=$(blkid | grep $label | awk -F':' '{print $1}') 
+
+if mount | grep $device &> /dev/null; then
+    echo $(date +"%Y-%m-%d-%H:%M:%S") "Backup device already mounted."
+else 
+    mkdir -p /media/$label
+    mount $device /media/$label
+fi
+
+while true; do
+    if [ ! -f "/media/$label/wallet.dat" ]; then
+        cp $wallet /media/$label
+    fi
+    inotifywait $wallet
+    cp $wallet /media/$label
+done
